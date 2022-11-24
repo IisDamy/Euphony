00a848d78c2a45e07b17bda227ac451be035c000
JPEGDAMY <109248005+IisDamy@users.noreply.github.com>
2022-11-24 16:48:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Blur from 'react-blur'
+
+
+
+
+
+
+
+
 
 function App() {
+  
+  console.log(Blur)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Blur blurRadius={15} img={'images/flumePic.jpg'} enableStyles className={'w-full h-screen m-4 mx-16'} />
+    
+     
+
+      
     </div>
   );
 }
